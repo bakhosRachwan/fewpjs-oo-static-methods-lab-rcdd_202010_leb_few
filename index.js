73d0cad8658e4d39b57ea7ec476e83c9ab7c6f1f
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-11 23:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, ''); 
+  }
+   static titleize(string){
+    return string.split(' ').map(x => {
+      if(x === 'a'  && !x[0] || x === 'an' || x == 'the' || x === 'but' || x === 'of' || x === 'and' || x === 'for' || x === 'at'|| x === 'by' || x === 'from'){return x}
+      else {
+        return x.charAt(0).toUpperCase() + x.slice(1)
+      }
+    }).join(' ')
+  }
 }
